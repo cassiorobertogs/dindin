d483e1a1da584d3547ccd5c93315a1d99baf69ed
cassiorgs <adv.cassioroberto@gmail.com>
2021-12-18 19:05:00 -0400

    01 erros

---
+++ dindin/src/main/java/entidades/conta.java
@@ -0,0 +1,16 @@
+package entidades;
+
+@Entity
+public class conta {
+	@Id
+	private Integer numero id;
+	@Colunm
+	private Integer timpo;
+	@Colonm
+	private Integer agencia;
+	@Colonm
+	private double saldo;
+	@Colonm
+	private String titular;
+	
+}
